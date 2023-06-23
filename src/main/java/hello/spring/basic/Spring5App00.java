396e1fb36abb008e65419de65f42db78b1c13441
nou <nou13n8e@gmail.com>
2023-06-23 11:35:14 +0900

    스프링 : 매개변수 활용

--- src/main/java/hello/spring/basic/Spring5App00.java
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -3,12 +3,14 @@ package hello.spring.basic;
 public class Spring5App00 {
     //첫번째 사례: 간단한 인삿말을 출력하는 프로그램 작성
     public static void main(String[] args){
-        //sayHello 메서르를 호출하기 위해서
+        //sayHello 메서드를 호출하기 위해서
         //Spring5App00의 객체화 필요!
         Spring5App00 app00=new Spring5App00();
-        app00.sayHello();
+        app00.sayHello("Spring");
     }
-    private void sayHello(){
-        System.out.println("Hello, World!!");
+    private void sayHello(String msg){
+        //매개변수를 활용하여 인삿말 변경 가능
+        System.out.println("Hello, "+msg+"!!");
+        //System.out.println("Hello, World!!");
     }
 }
