47921060dd61769ec732341341ce70454c5c5ea5
nou <nou13n8e@gmail.com>
2023-06-23 11:19:35 +0900

    스프링 : 메서드 분리

--- src/main/java/hello/spring/basic/Spring5App00.java
+++ src/main/java/hello/spring/basic/Spring5App00.java
@@ -3,7 +3,12 @@ package hello.spring.basic;
 public class Spring5App00 {
     //첫번째 사례: 간단한 인삿말을 출력하는 프로그램 작성
     public static void main(String[] args){
+        //sayHello 메서르를 호출하기 위해서
+        //Spring5App00의 객체화 필요!
+        Spring5App00 app00=new Spring5App00();
+        app00.sayHello();
+    }
+    private void sayHello(){
         System.out.println("Hello, World!!");
     }
-
 }
